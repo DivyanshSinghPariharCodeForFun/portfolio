a4b124e64a2319bcf34b56a72f6f33b33e323cdd
DivyanshWriteCode <divyanshdocode@gmail.com>
2025-08-04 22:04:44 +0530

    Best

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -49,11 +49,12 @@ function Button({
   const Comp = asChild ? Slot : "button";
 
   return (
-    <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
+    // <Comp
+    //   data-slot="button"
+    //   className={cn(buttonVariants({ variant, size, className }))}
+    //   {...props}
+    // />
+    <h1></h1>
   );
 }
 
